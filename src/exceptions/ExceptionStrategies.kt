497ccddf8b69d7f3d9b2a921abbe9cf247c8075f
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-05-22 22:24:44 -0300

    estrategias de excecoes (ocultando excecoes)

---
+++ src/exceptions/ExceptionStrategies.kt
@@ -0,0 +1,20 @@
+package exceptions
+
+fun main() {
+    a()
+}
+
+fun a() {
+    try {
+        b()
+    } catch (b: B) {
+        throw A(b)
+    }
+}
+
+fun b() {
+    throw B()
+}
+
+class A(cause: Throwable) : RuntimeException(cause)
+class B : RuntimeException()
