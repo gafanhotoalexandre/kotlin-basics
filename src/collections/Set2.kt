60deaf367ee2afbc7498a8b6632b46ce8b3ea7cb
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-05-28 23:44:19 -0300

    sets, equals e hashcode

---
+++ src/collections/Set2.kt
@@ -0,0 +1,29 @@
+package collections
+
+
+fun main() {
+    val dogs = setOf(
+        Dog("Evandro"),
+        Dog("Osvaldo"),
+        Dog("Osvaldo"),
+        Dog("Djalma"),
+        Dog("Maurício"),
+        Dog("Maurício"),
+    )
+
+    println(dogs)
+}
+
+data class Dog(private val name: String) {
+    override fun toString() = "Dog(name=$name, hashCode=${hashCode()})"
+
+//    override fun hashCode(): Int {
+//        return Objects.hash(name)
+//    }
+//
+//    override fun equals(other: Any?): Boolean {
+//        return if (other is Dog) {
+//            this.name == other.name
+//        } else false
+//    }
+}
