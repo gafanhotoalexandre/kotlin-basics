60e295b491e80ce67a754d47e6089f8701b1d248
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-05-28 23:06:25 -0300

    sets

---
+++ src/collections/Set.kt
@@ -0,0 +1,11 @@
+package collections
+
+fun main() {
+    val s1 = setOf(1, 2, 3)
+    val s2 = mutableSetOf("A", "b", "C", "c")
+    val s3 = hashSetOf("1A", "5B", "10C", "20D", "40E")
+
+    println(s1)
+    println(s2)
+    println(s3)
+}
