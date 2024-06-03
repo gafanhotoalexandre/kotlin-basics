d1b89fe07c587f4a8911a328e1f787a5145725d5
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-06-03 20:56:05 -0300

    propriedades em um enum

--- src/enums/Month.kt
+++ src/enums/Month.kt
@@ -6,17 +6,19 @@ fun main() {
     println(yearMonth)
 }
 
-enum class Month {
-    JANUARY,
-    FEBRUARY,
-    MARCH,
-    APRIL,
-    MAY,
-    JUNE,
-    JULY,
-    AUGUST,
-    SEPTEMBER,
-    OCTOBER,
-    NOVEMBER,
-    DECEMBER
+enum class Month(
+    val numberOfMonth: Int
+) {
+    JANUARY(1),
+    FEBRUARY(2),
+    MARCH(3),
+    APRIL(4),
+    MAY(5),
+    JUNE(6),
+    JULY(7),
+    AUGUST(8),
+    SEPTEMBER(9),
+    OCTOBER(10),
+    NOVEMBER(11),
+    DECEMBER(12)
 }
