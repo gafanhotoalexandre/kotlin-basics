cb48080b276f8b4af5f4598e76c1d933e77f5f1e
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-06-03 20:35:38 -0300

    conhecendo enums

---
+++ src/enums/Month.kt
@@ -0,0 +1,22 @@
+package enums
+
+fun main() {
+    val yearMonth = Month.MAY
+
+    println(yearMonth)
+}
+
+enum class Month {
+    JANUARY,
+    FEBRUARY,
+    MARCH,
+    APRIL,
+    MAY,
+    JUNE,
+    JULY,
+    AUGUST,
+    SEPTEMBER,
+    OCTOBER,
+    NOVEMBER,
+    DECEMBER
+}
