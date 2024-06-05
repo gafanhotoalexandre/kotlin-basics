2d2a5780ef4ad399966e423ab5cfa51ad259cbf8
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2024-06-04 22:10:10 -0300

    entendendo melhor sobre organizacao de codigo

---
+++ src/organization/p1/FileA.kt
@@ -0,0 +1,18 @@
+package organization.p1
+
+import organization.p2.*
+
+fun main() {
+    a1()
+    a2()
+    b1()
+    b2()
+}
+
+fun a1() {
+    println("a1()")
+}
+
+fun a2() {
+    println("a2()")
+}
